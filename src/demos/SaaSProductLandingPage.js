16dde7cfdee6a8765e0d3e0c1023e02825d69753
Shameekh Naveed <shameekh2002@gmail.com>
2024-08-05 00:55:35 +0500

    init

--- src/demos/SaaSProductLandingPage.js
+++ src/demos/SaaSProductLandingPage.js
@@ -3,14 +3,10 @@ import tw from 'twin.macro';
 import { css } from 'styled-components/macro'; //eslint-disable-line
 import AnimationRevealPage from 'helpers/AnimationRevealPage.js';
 import Hero from 'components/hero/TwoColumnWithVideo.js';
-import Features from 'components/features/ThreeColWithSideImage.js';
 import MainFeature from 'components/features/TwoColWithButton.js';
 import MainFeature2 from 'components/features/TwoColWithTwoHorizontalFeaturesAndButton.js';
 import FeatureWithSteps from 'components/features/TwoColWithSteps.js';
-import Pricing from 'components/pricing/ThreePlans.js';
-// import Testimonial from 'components/testimonials/TwoColumnWithImageAndRating.js';
 import FAQ from 'components/faqs/SingleCol.js';
-import GetStarted from 'components/cta/GetStarted';
 import Footer from 'components/footers/FiveColumnWithBackground.js';
 import heroScreenshotImageSrc from 'images/hero-screenshot-1.png';
 import macHeroScreenshotImageSrc from 'images/hero-screenshot-2.png';
